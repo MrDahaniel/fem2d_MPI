9fd62e362c989e9adb3ab7c0330143c66914183c
dnl <danieldavid2001@gmail.com>
2022-09-19 09:17:00 -0500

    memes

--- code/mpi.sh
+++ code/mpi.sh
@@ -6,16 +6,8 @@
 #SBATCH --ntasks=2         #Numero de procesos
 #SBATCH --tasks-per-node=2   #Numero de trabajos por nodo
 
-#Prepara el ambiente de trabajo
-#export I_MPI_PMI_LIBRARY=/usr/local/slurm/lib/libpmi.so
 
 module load devtools/mpi/openmpi/4.0.1
 modele load devtools/gcc/9.2.0
 
-# ulimit -l unlimited
-# export OMPI_MCA_btl=^openib
-
-mpic++ fem2d_poisson_mpi.cpp -o fem2d_mpi.out
-
-#Ejecuta el programa paralelo
-mpirun ./fem2d_mpi.out
+mpic++ fem2d_poisson_mpi.cpp -o fem2d_mpi.out && mpirun ./fem2d_mpi.out
